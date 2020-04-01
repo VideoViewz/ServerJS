f94f257df93a15739e1f1aaa5f9eb31ae634ecc5
Igor <igrr69@gmail.com>
2020-04-01 11:00:06 +0300

    Add video link for embeding,  Fix name of db prop,  remove e2e tests for now

--- src/video/interfaces/video.interface.ts
+++ src/video/interfaces/video.interface.ts
@@ -3,11 +3,11 @@ import { Document } from 'mongoose';
 export interface Video extends Document {
   readonly name: string;
   readonly user: string;
-  readonly class: string;
+  readonly course: string;
 }
 
 export class CreateVideoDto {
   readonly name: string;
   readonly user: string;
-  readonly class: string;
+  readonly course: string;
 }
--- src/video/schemas/video.schema.ts
+++ src/video/schemas/video.schema.ts
@@ -3,5 +3,5 @@ import * as mongoose from 'mongoose';
 export const VideoSchema = new mongoose.Schema({
   name: String,
   user: String,
-  class: String,
+  course: String,
 });
