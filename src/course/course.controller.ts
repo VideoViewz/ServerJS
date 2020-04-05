00ac402b6897d9f72fee8066c47c47c317db278b
Igor <igrr69@gmail.com>
2020-04-05 14:46:55 +0300

    Add retrival of all courses

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,19 +1,22 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
-import { CourseService } from './course.new-service';
-import { CourseDto } from './dto/course.dto';
-import { Course } from './entities/course.entity';
+import {Controller, Post, Body, Get} from '@nestjs/common';
+import {CourseService} from './course.new-service';
+import {CourseDto} from './dto/course.dto';
+import {Course} from './entities/course.entity';
 
 @Controller('course')
-export class CourseController {
-  constructor(private readonly courseService: CourseService) { }
+export class CourseController
+{
+  constructor (private readonly courseService: CourseService) {}
 
   @Post('new')
-  async newCourse(@Body() course: Course) {
+  async newCourse(@Body() course: Course)
+  {
     return await this.courseService.create(course);
   }
 
-  // @Get()
-  // async getAllCourses() {
-  //   return await this.courseService.getAllCourses();
-  // }
+  @Get()
+  async getAllCourses()
+  {
+    return await this.courseService.getAllCourses();
+  }
 }
