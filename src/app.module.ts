ebdba952e448e9b7855e20dd9a924ba5763512a1
Igor <igrr69@gmail.com>
2020-04-05 14:57:04 +0300

    Remove mongo from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {VideoModule} from './video/video.module';
-import {MongooseModule} from '@nestjs/mongoose';
 import {config} from 'dotenv';
 import {CourseModule} from './course/course.module';
 import {TypeOrmModule} from '@nestjs/typeorm';
@@ -12,15 +11,6 @@ config();
 @Module({
   imports: [
     VideoModule,
-    MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@videoviewzcluster0-tkxqx.mongodb.net/test?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: process.env.DB,
-        useCreateIndex: true,
-      },
-    ),
     TypeOrmModule.forRoot({
       type: 'postgres',
       ssl: {
